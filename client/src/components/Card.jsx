02702074eaffe175057a9a6e1f36f3702d6793a8
romica44 <romica44@hotmail.com>
2022-01-28 15:33:51 -0300

    add

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './Card.css';
 
-export default function Card ({name,image,temperaments, weightMin, weightMax}){
+export default function Card ({name,image,temperaments, weightMin, weightMax, life_span}){
     return (
        <div className='card' >
             <h1 className='info' >{name}</h1>
             <img src={image} alt={`${name}`} width='250px' heigth='200px' className='imageDog'/>
             <h3 className='info'>Temperaments: {temperaments}</h3>
             <h3 className='info'>Weight: {weightMin} - {weightMax} kg</h3> 
+            <h3 className='info'>Life Span: {life_span} - years</h3> 
                 <></>
             
         </div>
